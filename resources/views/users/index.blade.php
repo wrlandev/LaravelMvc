4263bb51359df643bc9ef1b3ca0d6609819f6ed3
Wrlan Silva <157828195+wrlandev@users.noreply.github.com>
2024-02-15 12:17:28 -0300

    Listando resgistro do bd na view com eloquent

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,10 @@
-<h1>olá</h1>
+<h1>Listagem</h1>
+
+<ul>
+    @foreach ($users as $user)
+        <li>
+            {{ $user->name }}
+            {{ $user->email }}
+        </li>
+    @endforeach
+</ul>
